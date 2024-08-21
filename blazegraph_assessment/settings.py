3b627e2b4269bae6448ef1c3b8d514c3c8446b71
mimran221992 <imran@immersive-fox.com>
2024-08-22 01:59:02 +0500

    settings cors

--- blazegraph_assessment/settings.py
+++ blazegraph_assessment/settings.py
@@ -51,9 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-CORS_ALLOW_ALL_ORIGINS = True
 
+]
+        
+CORS_ALLOWED_ORIGINS= ["http://localhost:3000"]                 
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'blazegraph_assessment.urls'
 
 TEMPLATES = [
